962225be2bef1a99ebfeb20224b8d12f01038bf2
sudip17x <sudip17x@gmail.com>
2025-11-24 19:16:06 +0530

    Fix signup, update backend code, auth.js bug fixed

--- routes/auth.js
+++ routes/auth.js
@@ -15,25 +15,31 @@ router.post("/signup", async (req, res) => {
 
     const hashed = await bcrypt.hash(password, 10);
 
-    await User.create({
+    // IMPORTANT FIX: Save the created user
+    const newUser = await User.create({
       name,
       email,
       password: hashed
     });
 
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign(
+      { id: newUser._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
 
-res.json({
-  message: "Signup success",
-  token,
-  user: {
-    id: newUser._id,
-    name: newUser.name,
-    email: newUser.email
-  }
-});
+    res.json({
+      message: "Signup success",
+      token,
+      user: {
+        id: newUser._id,
+        name: newUser.name,
+        email: newUser.email
+      }
+    });
 
   } catch (err) {
+    console.error("Signup Error:", err);
     res.status(500).json({ message: err.message });
   }
 });
